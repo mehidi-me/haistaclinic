66b4a8114159ea9bfa3d708707f1d2ce1a414c8d
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-08-08 01:57:13 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <body className={poppins.className}>{children}</body> */}
-      <body className={poppins.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
