e1891cabecafce7160466484a720233affce8ab0
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-08-08 00:44:14 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import {  Poppins } from "next/font/google";
 import "./style.css";
 
-const poppins = Poppins({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["300","400","500","600","700"]});
 
 export const metadata = {
   title: "Create Next App",
