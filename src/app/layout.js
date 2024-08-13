aad5289842c75ab7b952e1754e65000cf7bf2c9f
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-08-14 00:09:35 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import {  Poppins } from "next/font/google";
 import {  Bebas_Neue } from "next/font/google";
 import "slick-carousel/slick/slick.css";
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en"
      className={`${poppins.variable} ${bebasNeue.variable}`}
      >
+      <GoogleTagManager gtmId="GTM-WRSSXR4" />
       <body>{children}</body>
     </html>
   );
